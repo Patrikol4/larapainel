55d6b1527b57c85a8e66d0c4f130f3459509926d
Patrikol4 <doublecrt@gmail.com>
2021-04-18 16:06:20 -0300

    Instei as dependencias compilei tudo conectei ao mysql e instalei o inertia

--- config/database.php
+++ config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('postgres://bhpwfxog:MPiJQ28j1EpFA-J5XMXnUERR4BIHGNtc@motty.db.elephantsql.com:5432/bhpwfxog'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'bhpwfxog'),
+            'username' => env('DB_USERNAME', 'bhpwfxog'),
+            'password' => env('DB_PASSWORD', 'MPiJQ28j1EpFA-J5XMXnUERR4BIHGNtc'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
